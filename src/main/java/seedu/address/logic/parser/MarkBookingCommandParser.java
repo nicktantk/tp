82481259f066f1335de013546974b6aaf81a88f7
huiyi-Al <E1295583@u.nus.edu>
2025-10-16 21:53:46 +0800

    Update MarkBookingCommandParser.java

--- src/main/java/seedu/address/logic/parser/MarkBookingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkBookingCommandParser.java
@@ -1,21 +1,27 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.MarkBookingCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
-public class MarkBookingCommandParser implements Parser<MarkBookingCommand>{
-
-
-
+/**
+ * Parses the input argument
+ */
+public class MarkBookingCommandParser implements Parser<MarkBookingCommand> {
+   /**
+     * Parses the given {@code String} of arguments in the context of the ViewBookingCommand
+     * and returns a ViewBookingCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public MarkBookingCommand parse(String args) throws ParseException {
 
         try {
             Index index = ParserUtil.parseIndex(args);
             return new MarkBookingCommand(index);
-        }catch (ParseException pe) {
+        } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkBookingCommand.MESSAGE_USAGE), pe);
         }
