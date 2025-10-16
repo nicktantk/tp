f2b68989a519aa9639ade6efd96d7d2b8d3b4bf7
huiyi-Al <E1295583@u.nus.edu>
2025-10-16 21:46:13 +0800

    Update MarkBookingCommand.java

--- src/main/java/seedu/address/logic/commands/MarkBookingCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkBookingCommand.java
@@ -1,16 +1,18 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.booking.Booking;
 
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
-
+/**
+ *Mark the status of a booking section in a given index
+ */
 public class MarkBookingCommand extends Command {
 
     public static final String COMMAND_WORD = "markbooking";
@@ -22,11 +24,11 @@ public class MarkBookingCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Booking %1$s has been marked";
     public static final String MESSAGE_NOTFOUND = "Invalid booking ID: %1$s";
-    public static final String MESSAGE_DUPLICATE= "you have already marked this booking";
+    public static final String MESSAGE_DUPLICATE = "you have already marked this booking";
 
     private final Index bookingindex;
 
-    public MarkBookingCommand(Index bookingindex) {this.bookingindex = bookingindex;}
+    public MarkBookingCommand(Index bookingindex) { this.bookingindex = bookingindex; }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
@@ -38,7 +40,7 @@ public class MarkBookingCommand extends Command {
         }
 
         Booking booking = lastShownList.get(bookingindex.getZeroBased());
-        if (booking.isDone()){
+        if (booking.isDone()) {
             throw new CommandException(MESSAGE_DUPLICATE);
         }
         Booking markedbooking = booking.markBooking();
