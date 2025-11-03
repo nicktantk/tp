143bb4576c336c3da03fc831c2dc2b4f04922d70
Jyang1206 <e1408891@u.nus.edu>
2025-11-03 22:37:15 +0800

    Update Mark booking message

--- src/main/java/seedu/address/logic/commands/UnmarkBookingCommand.java
+++ src/main/java/seedu/address/logic/commands/UnmarkBookingCommand.java
@@ -18,7 +18,7 @@ public class UnmarkBookingCommand extends Command {
 
     public static final String COMMAND_WORD = "unmarkbooking";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Unmarks a booking as 'Not Paid'.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks a booking as ' Unpaid'.\n"
             + "Parameters: "
             + "INDEX (must be a positive integer) \n"
             + "Example: " + COMMAND_WORD + " 1";
